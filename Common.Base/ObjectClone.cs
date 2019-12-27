ac7537a447c6f488328efe3ee601dc8acab557a0
简约蚂蚁 <767649181@qq.com>
2019-12-27 14:45:06 +0800

    深复制

---
+++ Common.Base/ObjectClone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Common.Base
+{
+    public static class ObjectClone
+    {
+        public static T DeepthCloneByBinaryFormatter<T>(T obj)
+        {
+            var ret = default(T);
+
+            if (object.ReferenceEquals(obj, null) == false)
+            {
+                var formatter = new BinaryFormatter();
+                using (var stream = new MemoryStream())
+                {
+                    formatter.Serialize(stream, obj);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    ret = (T)formatter.Deserialize(stream);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
